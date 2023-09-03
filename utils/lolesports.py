ebe65a1a08fb129596146c351f87012fa237c9e3
Alex Zhu <ayjzhu@gmail.com>
2023-09-02 21:52:07 -0700

    Added API support for get tournaments and standings

--- utils/lolesports.py
+++ utils/lolesports.py
@@ -3,6 +3,7 @@ import requests
 from dotenv import load_dotenv
 from enum import Enum
 import pandas as pd
+from typing import Optional, Union, List 
 
 load_dotenv()
 # API_BASE = os.getenv('API_BASE')
@@ -136,7 +137,7 @@ class LolEsports:
             return leagues
     
     # create a helper function to process the league data and get the sub leagues
-    def get_sub_leagues(self, leagues: list) -> tuple:
+    def _get_sub_leagues(self, leagues: list) -> tuple:
         """ A helper to process the leagues data and sort them into various sub leagues
 
         Parameters
@@ -175,18 +176,168 @@ class LolEsports:
         else:
             return major_leagues.to_dict(orient='records'), popular_leagues.to_dict(orient='records'), primary_leagues.to_dict(orient='records')
 
-    
+    def get_tournaments(self, region_ids: Union[int, List[int]]) -> dict:
+        """Get all the esports tournaments from a region
+
+        Parameters
+        ----------
+        region_ids: `int` or `list` of `int`
+            The region_id(s) to get the tournaments from.
+
+        Returns
+        -------
+        tournaments: `dict`
+            A dictionary of esports tournaments
+        ---
+        """
+        if isinstance(region_ids, int):
+            league_ids = [region_ids]  # Convert a single league_id to a list
+        elif isinstance(region_ids, list):
+            league_ids = region_ids  # Use the provided list of league_ids
+        else:
+            raise ValueError("Invalid parameter type. Expected int or list of int.")
+
+        payload = {
+            'hl': 'en-US',
+            'leagueId': ','.join(map(str, league_ids))
+        }
+        url = f'{self.api_base}/getTournamentsForLeague'
+        response = requests.get(url, params=payload, headers=self.headers)
+        print(response)
+        print(response.url)
+        return response.json()
+
+    # create a helper function to extract tournaments by timeframe
+    def _extract_tournaments_by_timeframe(self, data: dict, timeframe: str) -> list:
+        """ A helper to extract tournaments by timeframe
+
+        Parameters
+        ----------
+        data: `dict`
+            A dictionary of esports tournaments
+        timeframe: `str`
+            The timeframe to extract the tournaments from
+
+        Returns
+        -------
+        matching_tournaments: `list`
+            A list of tournaments that match the timeframe
+        ---
+        """
+
+        matching_tournaments = []
+        tournaments_data = data['data']['leagues']
+
+        for item in tournaments_data:
+            tournaments = item.get('tournaments', [])
+            for tournament in tournaments:
+                slug = tournament.get('slug', '')
+                if timeframe in slug:
+                    matching_tournaments.append(tournament)
+
+        return matching_tournaments
+
+    def get_standings(self, tournament_id: Union[int, List[int]]) -> List[dict]:
+        """Get the standings of a tournament
+
+        Parameters
+        ----------
+        tournament_id: `int` or `list` of `int`
+            The tournament_id(s) to get the standings from.
+
+        Returns
+        -------
+        rankings: `list` of `dict`
+            A list of standings for each tournament
+        ---
+        """
+        if isinstance(tournament_id, int):
+            tournament_id = [tournament_id]  # Convert a single league_id to a list
+        elif isinstance(tournament_id, list):
+            pass  # Use the provided list of league_ids
+        else:
+            raise ValueError("Invalid parameter type. Expected int or list of int.")
+        payload = {
+            'hl': 'en-US',
+            'tournamentId' : ','.join(map(str, tournament_id)),
+        }
+        url = f'{self.api_base}/getStandingsV3'
+        response = requests.get(url, params=payload, headers=self.headers)
+        print(response)
+        standings = response.json()['data']['standings']
+        rankings = []
+        for standing in standings:
+            slugs = standing['slug'].split('_')   # get the season name to use as key
+            season = ' '.join(slug.capitalize() for slug in slugs)
+            stages = standing['stages']                    # only regular season has rankings
+            ranking = stages[0]['sections'][0]['rankings'] # stage 0: regular season; only 1 section
+            rankings.append({season:ranking})
+        return rankings
+
+    def display_standings(self, region_ids: Union[int, List[int]], timeframe: str, to_str: bool = False) -> Optional[str]:
+        """Display the standings of a tournament
+        
+        Parameters
+        ----------
+        region_ids: `int` or `list` of `int`
+            The region_id(s) to get the tournaments from.
+        timeframe: `str`
+            The timeframe to extract the tournaments from
+        to_str: `bool`
+            Whether to return the standings as a string or print it
+
+        Returns
+        -------
+        standings_str: `str`
+            The standings as a string
+        ---
+        """
+        # this function calls get_tournaments, extract_tournaments_by_timeframe, get_standings and then display the standings
+        # get the tournaments
+        tournaments = self.get_tournaments(region_ids)
+        # get the tournaments for the timeframe
+        matching_tournaments = self._extract_tournaments_by_timeframe(tournaments, timeframe)
+        # get the matching ids
+        matching_ids = [tournament['id'] for tournament in matching_tournaments]
+        # get the standings
+        standings = self.get_standings(matching_ids)
+        # display the standings
+        standings_str = ""
+        for standing in standings:
+            for standing_name, rankings in standing.items():
+                standings_str += f"\n{standing_name}:"
+                for ranking in rankings:    # ranking within each league
+                    ordinal = ranking['ordinal']
+                    team = ranking['teams'][0] # only one team per slot
+                    name = team['name']
+                    code = team['code']
+                    wins = team['record']['wins']
+                    losses = team['record']['losses']
+                    standings_str += f"\n{ordinal}. {name} ({code}): {wins}-{losses}"
+            standings_str += "\n"
+        if to_str:
+            return standings_str
+        else:
+            print(standings_str)
 
 if __name__ == "__main__":
     lolesports = LolEsports(region=Region.LCS)
+    # # test get_live
     # print(lolesports.get_live())
+    # # test get_schedule
     # print(lolesports.get_schedule(Region.LCK)['data']['schedule']['events'][-1])
-    ## print the first 16 leagues by name
+    # # test get_leagues
+    # # print the first 16 leagues by name
     # leagues = lolesports.get_leagues(is_sorted=True)
     # for league in leagues[:16]:
     #     print(league['name'])
-    sub_leagues = lolesports.get_sub_leagues(lolesports.get_leagues(is_sorted=True))[0]
-    # print the major, popular and pirmary leagues by name
-    for league in sub_leagues:
-        print(league['name'])
-    
+    # # test get_sub_leagues
+    # sub_leagues = lolesports.get_sub_leagues(lolesports.get_leagues(is_sorted=True))[0]
+    # for league in sub_leagues:
+    #     print(league['name'])   # print the major, popular and pirmary leagues by name
+
+    # test get_tournaments, _extract_tournaments_by_timeframe, and get_standings
+    major_regions_ids = [region.value for region in Region][:4]
+    timeframe = "summer_2023"
+    # test display_standings
+    lolesports.display_standings(major_regions_ids, timeframe, to_str=False)
